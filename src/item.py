3752823364a8d42445a6698bb3b5b10ce1dca187
Sergey Semyonov <evlx@mail.ru>
2023-10-05 21:43:58 +0300

    add hw_1

--- src/item.py
+++ src/item.py
@@ -5,6 +5,15 @@ class Item:
     pay_rate = 1.0
     all = []
 
+    @classmethod
+    def set_discount_rate(cls, rate):
+        """
+        Устанавливает скидку для всех товаров.
+
+        :param rate: Новое значение скидки.
+        """
+        cls.pay_rate = rate
+
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
@@ -13,7 +22,11 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        Item.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -21,10 +34,10 @@ class Item:
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price * self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price *= self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import Item
+
+
+def test_calculate_total_price():
+    item = Item('apple', 10.0, 5)
+    assert item.calculate_total_price() == 50.0
+
+
+def test_apply_discount():
+    item = Item('apple', 10.0, 5)
+    item.apply_discount()
+    assert item.price == 10.0 * item.pay_rate
