cbd1fe61d986dc3c52101e75c23b2a636e3c528e
Sergey Semyonov <evlx@mail.ru>
2023-10-10 17:36:09 +0300

    add hw_2 (имеется проблема с путями к CSV файлу)

--- src/item.py
+++ src/item.py
@@ -1,3 +1,7 @@
+import csv
+import os
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -9,7 +13,6 @@ class Item:
     def set_discount_rate(cls, rate):
         """
         Устанавливает скидку для всех товаров.
-
         :param rate: Новое значение скидки.
         """
         cls.pay_rate = rate
@@ -17,21 +20,30 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
 
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        if len(new_name) <= 10:
+            self.__name = new_name
+        else:
+            self.__name = new_name[:10]
+
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
         return self.price * self.quantity
@@ -41,3 +53,15 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        csv_path = os.path.join(os.path.dirname(__file__), '..', 'src', path)
+        with open(csv_path, encoding="cp1251") as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], float(row['price']), int(row['quantity']))
+
+    @staticmethod
+    def string_to_number(string):
+        return int(string.split(',')[0])
