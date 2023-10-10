f78622bcc256dc4d4ca2de87463a342d4b104cdc
Sergey Semyonov <evlx@mail.ru>
2023-10-10 21:46:30 +0300

    add tests hw_2

--- tests/test_item.py
+++ tests/test_item.py
@@ -13,3 +13,28 @@ def test_apply_discount():
     item = Item('apple', 10.0, 5)
     item.apply_discount()
     assert item.price == 10.0 * item.pay_rate
+
+
+@pytest.fixture
+def sample_item():
+    return Item("Sample Item", 10.0, 5)
+
+
+def test_name_property(sample_item):
+    assert sample_item.name == "Sample Item"
+
+
+def test_name_setter(sample_item):
+    sample_item.name = "New Item Name"
+    assert sample_item.name == "New Item N"
+
+
+def test_instantiate_from_csv():
+    csv_file = "tests/test_data.csv"
+
+    Item.instantiate_from_csv(csv_file)
+    assert len(Item.all) == 2
+
+
+def test_string_to_number():
+    assert Item.string_to_number("10,5") == 10
